e3539350400fdc70417800ca41ef9b19375aa7b8
svelezp <svelezp10@eafit.edu.co>
2023-05-30 17:05:25 -0500

    envío de sms

--- back/notificador/smsSend.py
+++ back/notificador/smsSend.py
@@ -1,26 +1,28 @@
 import requests
 
-# Define the endpoint URL
-url = "https://prod-187.westus.logic.azure.com:443/workflows/4a07565298064a54baddd1e5ce62d3ea/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=pd0-NMUTK0RzU-3MpxvzMxwhuzvLmSWk4z6nENy4StI"
 
-# Define the request payload (data to send in the POST request)
-payload = {
-    "phoneAdress": "(+57)3168177190",
-    "content": "Succesful test my friend",
-}
+def sendSMS(phoneAdress, content):
+    # Define the endpoint URL
+    url = "https://prod-187.westus.logic.azure.com:443/workflows/4a07565298064a54baddd1e5ce62d3ea/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=pd0-NMUTK0RzU-3MpxvzMxwhuzvLmSWk4z6nENy4StI"
 
-# Send the POST request
-response = requests.post(url, json=payload)
+    # Define the request payload (data to send in the POST request)
+    payload = {
+        "phoneAdress": phoneAdress,
+        "content": content,
+    }
 
-# Process the response
-if response.status_code == 202:
-    # Request was successful
-    print("POST request successful.")
-    print("Response content:", response.text)
-else:
-    # Request failed
-    print("POST request failed. Status code:", response.status_code)
-    print("Error message:", response.text)
+    # Send the POST request
+    response = requests.post(url, json=payload)
+
+    # Process the response OPCIONAL, se puede quitar
+    if response.status_code == 202:
+        # Request was successful
+        print("POST request successful.")
+        print("Response content:", response.text)
+    else:
+        # Request failed
+        print("POST request failed. Status code:", response.status_code)
+        print("Error message:", response.text)
 
 def descifrado_cesar(texto_cifrado, desplazamiento):
     resultado = ""
@@ -36,3 +38,5 @@ def descifrado_cesar(texto_cifrado, desplazamiento):
         else:
             resultado += caracter
     return resultado
+
+sendSMS("(+57)3168177190", "Succesful test my friend")
